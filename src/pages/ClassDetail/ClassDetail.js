f3c2c03b417f80ed8b88d9befa52aa234825993f
jooney <jooneybadger@gmail.com>
2020-11-30 19:34:48 +0900

    wip

--- src/pages/ClassDetail/ClassDetail.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class ClassDetail extends Component {
-  render() {
-    return <div></div>;
-  }
-}
-
-export default ClassDetail;
--- src/pages/ClassLists/ClassLists.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class ClassLists extends Component {
-  render() {
-    return <div></div>;
-  }
-}
-
-export default ClassLists;
