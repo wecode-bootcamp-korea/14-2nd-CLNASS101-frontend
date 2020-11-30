68e38213f7eecaac212089bad6f727b48d0f27cb
Xednicoder <kaaiinn4@gmail.com>
2020-12-01 03:00:28 +0900

    Add : Navigation Bar

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const APISH = "/Data/SH/data.json";
-export const APIHS = "/Data/HS/data.json";
-export const APIYJ = "/Data/YJ/data.json";
+export const APISH = '/data/SH/myProfile.json';
+export const APIHS = '/Data/HS/data.json';
+export const APIYJ = '/Data/YJ/data.json';
